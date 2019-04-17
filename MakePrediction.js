db25d5782aab2940eb2c5855ea4a1422e924d56e
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-04-17 10:45:14 -0400

    Add a TextInput

--- MakePrediction.js
+++ MakePrediction.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, TextInput } from 'react-native';
 
-const MakePrediction = props => {
-  return (
-    <Text>
-      placeholder
-    </Text>
-  )
+const style = {
+  height: 100,
+  width: 200,
+  fontSize: 60,
+  borderColor: 'black',
+  borderWidth: 2,
+  borderStyle: 'dashed'
+}
+class MakePrediction extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {}
+  }
+
+  render(){
+    return <TextInput
+      style={style}
+      onChangeText={(text) => this.setState({text})}
+    />
+  }
 }
 
 MakePrediction.propTypes = {
