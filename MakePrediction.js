c17075eb16eaeefe4b109d269018b3f1a48a5bcd
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-04-18 10:33:53 -0400

    Add a button

--- MakePrediction.js
+++ MakePrediction.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { AppRegistry, TextInput, View } from 'react-native';
+import { AppRegistry, TextInput, View, Button } from 'react-native';
 
 const style = {
   minHeight: 45,
@@ -29,19 +29,24 @@ class MakePrediction extends Component{
       <TextInput
         multiline = {true}
         style={{...style, marginBottom: 10}}
-        placeholder="A thing will happen"
+        placeholder='A thing will happen'
         value={this.state.text}
         onChangeText={(text) => this.setState({text})}
       />
       <TextInput
         multiline = {true}
         style={style}
-        placeholder=".5"
+        placeholder='.5'
         keyboardType = 'numeric'
         value={this.state.number && !isNaN(this.state.number) ? String(this.state.number) : ''}
         onChangeText={(text) => {
           if(text.length) this.setState({number: textToNumber(text)})
         }}
+      />
+      <Button
+        title='Save'
+        color='#B1D4B4'
+        onPress={() => {}}
       />
     </>
   }
