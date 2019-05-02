e1a5213283f5b059bb26d44fc1a807d61c733b75
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-05-02 15:45:04 -0400

    Refactor styles

--- MakePrediction.js
+++ MakePrediction.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { AppRegistry, TextInput, View, Button } from 'react-native';
-
-const style = {
-  minHeight: 45,
-  width: 250,
-  fontSize: 40,
-  borderColor: 'black',
-  borderWidth: 2,
-  borderStyle: 'dashed',
-}
+import { AppRegistry, TextInput, Button } from 'react-native'
+import styles from './styles'
 
 function textToNumber(text){
   return text.replace(/[^\d.]+/g, '')
@@ -28,14 +20,14 @@ class MakePrediction extends Component{
     return <>
       <TextInput
         multiline = {true}
-        style={{...style, marginBottom: 10}}
+        style={{...styles.input, marginBottom: 10}}
         placeholder='A thing will happen'
         value={this.state.text}
         onChangeText={(text) => this.setState({text})}
       />
       <TextInput
         multiline = {true}
-        style={style}
+        style={styles.input}
         placeholder='.5'
         keyboardType = 'numeric'
         value={this.state.number && !isNaN(this.state.number) ? String(this.state.number) : ''}
--- styles.js
+++ styles.js
@@ -8,6 +8,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 10,
   },
+  input: {
+    minHeight: 45,
+    width: 250,
+    fontSize: 40,
+    borderColor: 'black',
+    borderWidth: 2,
+    borderStyle: 'dashed',
+  }
 })
 
 module.exports = styles
